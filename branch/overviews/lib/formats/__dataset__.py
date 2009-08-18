681e5c917272c9158cd1c3c7163da5c15c524b56
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2009-08-18 06:11:50 +0000

    Meged from trunk -__dataset__.py: Rewritten to expose "metadata" as a property with get & set methods. Added fileinfo so callers can walk the filesystem w/out having to extract metadata

--- branch/overviews/lib/formats/__dataset__.py
+++ branch/overviews/lib/formats/__dataset__.py
@@ -4,8 +4,9 @@ Base Dataset class
 Defines the metadata fields and populates some basic info
 '''
 
+import os,time,sys
+import UserDict 
 import utilities, uuid
-import os,time
 import UserDict 
 
 #Import fieldnames
@@ -14,19 +15,33 @@ import __fields__
 class Dataset(object):
     '''A base Dataset class'''
     def __new__(self,f):
-        #Initialise the object
+        ##Initialise the class object
         self=object.__new__(self)
+        
+        self._metadata_set=False
 
-        #Initialise the fields & metadata properties
+        ##Populate some basic info
+        self.fileinfo=utilities.FileInfo(f)
+        #self.guid=str(uuid.uuid4())
+        #Make the guid reproducible based on filename
+        self.guid=str(uuid.uuid3(uuid.NAMESPACE_DNS,f))
+        self.fileinfo['filename']=os.path.basename(f)
+        self.fileinfo['filepath']=f
+        self.fileinfo['guid']=self.guid
+
+        ##Initialise the fields & metadata properties
         self.fields=idict(__fields__.fields)#We don't want any fields added/deleted
         self._metadata={}
-        for field in __fields__.fields:self._metadata[field]=''
+        for field in __fields__.fields:
+            if field in self.fileinfo:self._metadata[field]=self.fileinfo[field]
+            else:self._metadata[field]=''
         self._metadata=idict(self._metadata) #We don't want any fields added/deleted
 
-        #Populate some basic fields
-        self._metadata['filepath']=f
-
         return self
+
+    def __getmetadata__(self):
+        '''In case subclasses don't override...'''
+        return self._metadata
 
     def __classproperty__(fcn):
         try:return property( **fcn() )
@@ -37,7 +52,10 @@ class Dataset(object):
         '''The metadata property.'''
 
         def fget(self):
-            return self._metadata
+            if self._metadata_set:
+               return self._metadata
+            else:
+               return self.__getmetadata__()
 
         def fset(self, key, value):
             self._metadata[key] = value
@@ -55,7 +73,6 @@ class idict(UserDict.IterableUserDict):
        modified from http://code.activestate.com/recipes/498072/
        to inherit UserDict.IterableUserDict
     '''
-
     def __setitem__(self, key, val):
         if key in self.data.keys():
             self.data[key]=val
@@ -66,5 +83,6 @@ class idict(UserDict.IterableUserDict):
 
     def pop(self, key):
         raise KeyError("Can't delete keys")
+
     def popitem(self):
         raise KeyError("Can't delete keys")
