1c055e81153008ecddac7492682fdebd82569a8e
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2010-02-19 04:59:58 +0000

    Adding the build tools

---
+++ build/buildmetageta.nsi
@@ -0,0 +1,160 @@
+
+    ######################################################################
+    #      NSIS Installation Script
+    #       
+    #       This install script requires the following symbols to be 
+    #       defined via the /D commandline switch. It is usually run
+    #       by the buildmetageta.py script:
+    #       
+    #       APP_DIR  Relative path to the MetaGETA code
+    #       VERSION  N.N.N.N format version number
+    #       OUTPATH  installer filepath
+    #       
+    #       Example:
+    #       makensis /DVERSION=1.2.0.123 /DOUTPATH=..\downloads\metageta-1.2-setup.exe /DAPP_DIR=tmp buildmetageta.nsi
+    #       
+    ######################################################################
+    !define /date YEAR "%Y"
+
+    !define APP_NAME "MetaGETA"
+    !define COMP_NAME "Department of Environment, Heritage, Water and the Arts"
+    !define WEB_SITE "http://code.google.com/p/metageta"
+    !define COPYRIGHT "${COMP_NAME}  © ${YEAR}"
+    !define DESCRIPTION "MetaGETA installer (Metadata Gathering, Extraction and Transformation Application)"
+    !define LICENSE_TXT "include\license.rtf"
+    !define MUI_WELCOMEFINISHPAGE_BITMAP "graphics\installer.bmp"
+    !define REG_START_MENU "Start Menu Folder"
+    !define MUI_ICON "graphics\metageta.ico"
+
+    var StartMenuFolder
+    
+    ######################################################################
+
+    SetCompressor LZMA
+    Name "${APP_NAME}"
+    Caption "${APP_NAME}"
+    OutFile "${OUTPATH}"
+    BrandingText "${APP_NAME}"
+    XPStyle on
+
+    ######################################################################
+
+    !define UNINSTALL_PATH "Software\Microsoft\Windows\CurrentVersion\Uninstall\${APP_NAME}"
+    !define MULTIUSER_EXECUTIONLEVEL Highest
+    !define MULTIUSER_MUI
+    !define MULTIUSER_INSTALLMODE_COMMANDLINE
+    !define MULTIUSER_INSTALLMODE_INSTDIR "${APP_NAME}"
+    !define MULTIUSER_INSTALLMODE_DEFAULT_REGISTRY_KEY "${UNINSTALL_PATH}"
+    !define MULTIUSER_INSTALLMODE_DEFAULT_REGISTRY_VALUENAME "UninstallString"
+    !define MULTIUSER_INSTALLMODE_INSTDIR_REGISTRY_KEY "${UNINSTALL_PATH}"
+    !define MULTIUSER_INSTALLMODE_INSTDIR_REGISTRY_VALUENAME "InstallLocation"
+    ;!define REG_ROOT "HKCU"
+    !define REG_ROOT "SHCTX"
+
+    !include MultiUser.nsh
+    !include "MUI.nsh"
+
+    !define MUI_ABORTWARNING
+    !define MUI_UNABORTWARNING
+
+    ######################################################################
+
+    VIProductVersion  "${VERSION}"
+    VIAddVersionKey "ProductName"  "${APP_NAME}"
+    VIAddVersionKey "CompanyName"  "${COMP_NAME}"
+    VIAddVersionKey "LegalCopyright"  "${COPYRIGHT}"
+    VIAddVersionKey "FileDescription"  "${DESCRIPTION}"
+    VIAddVersionKey "FileVersion"  "${VERSION}"
+
+    ######################################################################
+    
+    !insertmacro MUI_PAGE_WELCOME
+    !insertmacro MUI_PAGE_LICENSE "${LICENSE_TXT}"
+    !insertmacro MULTIUSER_PAGE_INSTALLMODE
+    !insertmacro MUI_PAGE_DIRECTORY
+
+    !ifdef REG_START_MENU
+        !define MUI_STARTMENUPAGE_DEFAULTFOLDER "${APP_NAME}"
+        !define MUI_STARTMENUPAGE_REGISTRY_ROOT "${REG_ROOT}" 
+        !define MUI_STARTMENUPAGE_REGISTRY_KEY "${UNINSTALL_PATH}"
+        !define MUI_STARTMENUPAGE_REGISTRY_VALUENAME "${REG_START_MENU}"
+        !insertmacro MUI_PAGE_STARTMENU Application $StartMenuFolder
+    !endif
+
+    !insertmacro MUI_PAGE_INSTFILES
+    !insertmacro MUI_PAGE_FINISH
+    !insertmacro MUI_UNPAGE_CONFIRM
+    !insertmacro MUI_UNPAGE_INSTFILES
+    !insertmacro MUI_UNPAGE_FINISH
+    !insertmacro MUI_LANGUAGE "English"
+
+    ######################################################################
+
+    Section -MainProgram
+        ;${INSTALL_TYPE}
+        SetOverwrite ifnewer
+        SetOutPath $INSTDIR
+        File /r "${APP_DIR}\*"
+        SetOutPath $INSTDIR
+        File /r /x *.pyc /x *.pyo /x pythonwin "..\OSGeo4W"
+    SectionEnd
+
+    
+    ######################################################################
+
+    Section -Icons_Reg
+        SetOutPath "$INSTDIR"
+        WriteUninstaller "$INSTDIR\uninstall.exe"
+
+        !ifdef REG_START_MENU
+            !insertmacro MUI_STARTMENU_WRITE_BEGIN Application
+                CreateDirectory "$SMPROGRAMS\$StartMenuFolder"
+                CreateShortCut  "$SMPROGRAMS\$StartMenuFolder\Run Crawler.lnk" "$INSTDIR\metageta\runcrawler.bat" "" "$INSTDIR\metageta\lib\wm_icon.ico" 0 SW_SHOWMINIMIZED
+                CreateShortCut  "$SMPROGRAMS\$StartMenuFolder\Run Transform.lnk" "$INSTDIR\metageta\runtransform.bat" "" "$INSTDIR\metageta\lib\wm_icon.ico" 0 SW_SHOWNORMAL
+                CreateShortCut  "$SMPROGRAMS\$StartMenuFolder\MetaGETA Shell.lnk" "$INSTDIR\metageta\metageta-shell.bat" "" "$SYSDIR\cmd.exe" 0 SW_SHOWNORMAL
+                CreateShortCut  "$SMPROGRAMS\$StartMenuFolder\${APP_NAME} API Documentation.lnk" "$INSTDIR\${APP_NAME}\doc\index.html" "" "$SYSDIR\SHELL32.dll" 23 SW_SHOWMAXIMIZED
+                CreateShortCut  "$SMPROGRAMS\$StartMenuFolder\Uninstall ${APP_NAME}.lnk" "$INSTDIR\uninstall.exe"
+
+                !ifdef WEB_SITE
+                    WriteIniStr "$INSTDIR\${APP_NAME} website.url" "InternetShortcut" "URL" "${WEB_SITE}"
+                    CreateShortCut "$SMPROGRAMS\$StartMenuFolder\${APP_NAME} Website.lnk" "$INSTDIR\${APP_NAME} website.url" "" "$SYSDIR\SHELL32.dll" 13 SW_SHOWMAXIMIZED
+                !endif
+            !insertmacro MUI_STARTMENU_WRITE_END
+        !endif
+
+        WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "DisplayName" "${APP_NAME}"
+        WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "UninstallString" "$INSTDIR\uninstall.exe"
+        WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "DisplayIcon" "$INSTDIR\${APP_NAME}\lib\wm_icon.ico"
+        WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "DisplayVersion" "${VERSION}"
+        WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "Publisher" "${COMP_NAME}"
+
+        !ifdef WEB_SITE
+            WriteRegStr ${REG_ROOT} "${UNINSTALL_PATH}"  "URLInfoAbout" "${WEB_SITE}"
+        !endif
+    SectionEnd
+
+    ######################################################################
+
+    Section Uninstall
+        RMDir /r "$INSTDIR"
+        !ifdef REG_START_MENU
+            !insertmacro MUI_STARTMENU_GETFOLDER Application $StartMenuFolder
+            RMDir /r "$SMPROGRAMS\$StartMenuFolder"
+        !endif
+        DeleteRegKey ${REG_ROOT} "${UNINSTALL_PATH}"
+    SectionEnd
+
+    ######################################################################
+
+    ;Installer Functions
+    Function .onInit
+      !insertmacro MULTIUSER_INIT
+    FunctionEnd
+
+    ;Uninstaller Functions
+    Function un.onInit
+      !insertmacro MULTIUSER_UNINIT
+    FunctionEnd
+
+
+    
---
+++ build/buildmetageta.py
@@ -0,0 +1,259 @@
+# -*- coding: latin-1 -*-
+import os,sys,shutil,glob,tempfile,zipfile as zip
+
+def main():
+    svn=which('svn')
+    makensis=which('makensis')
+
+    if not svn:
+        print 'Install an SVN command line client (e.g. http://www.sliksvn.com) or ensure that it is on your PATH'
+        raw_input('Press enter to exit.')
+        sys.exit(1)
+
+    if not makensis:
+        print 'Install NSIS (http://nsis.sourceforge.net) or ensure that it is on your PATH'
+        raw_input('Press enter to exit.')
+        sys.exit(1)
+
+    if len(sys.argv)>1:
+        vers=sys.argv[1]
+    else:
+        try:vers = raw_input('Enter the version to build, options are \n1.N (eg. 1.1), curr (latest release),  trunk (unstable developement):  ')
+        except:sys.exit(0)#vers = 'trunk'
+        if vers in ['curr','']:
+            cmd='svn ls http://metageta.googlecode.com/svn/tags'
+            exit_code,stdout,stderr=runcmd(cmd)
+            if exit_code != 0:
+                if stderr:    print stderr
+                elif stdout:  print stdout
+                else :        print 'SVN command failed'
+                raw_input('Press enter to exit.')
+                sys.exit(exit_code)
+            else:
+                vers=stdout.strip().split()[-1][:-1]
+
+    cd = os.path.abspath(os.path.dirname(sys.argv[0]))
+    td = os.path.dirname(cd)
+
+    os.chdir(cd)
+    tmp=tempfile.mkdtemp(dir=cd)
+
+    ##########################################################
+    ##Get revision
+    if vers == 'trunk':repo='trunk'
+    else:repo='tags/'+vers
+        
+    cmd='svn info http://metageta.googlecode.com/svn/%s'%repo
+    exit_code,stdout,stderr=runcmd(cmd)
+    if exit_code != 0:
+        print stderr
+        raw_input('Press enter to exit.')
+        cleanup(tmp)
+        sys.exit(exit_code)
+        
+    for line in stdout.split('\n'):
+        line=line.split(':')
+        if line[0].strip()=='Last Changed Rev':
+            rev=line[1].strip()
+            break
+
+    if vers == 'trunk':
+        outfile='trunk-rev'+rev
+        vers='0.0.0.'+rev
+    else:
+        outfile=vers
+        vers=vers+'.0.'+rev
+
+    ##########################################################
+    print 'Cleaning up compiled objects'
+    for pyco in rglob(td,'*.py[c|o]'):
+        os.remove(pyco)
+
+    ##########################################################
+    print 'Exporting from SVN repo'
+    cmd='svn export -q --force http://metageta.googlecode.com/svn/%s %s/metageta'%(repo,tmp)
+    exit_code,stdout,stderr=runcmd(cmd)
+    if exit_code != 0:
+        if stderr:    print stderr
+        elif stdout:  print stdout
+        else :        print 'SVN export failed'
+        cleanup(tmp)
+        raw_input('Press enter to exit.')
+        sys.exit(exit_code)
+
+    ##########################################################
+    f=open('%s/version.txt'%tmp,'w').write('Version: %s'%vers)
+    for f in glob.glob('include/*'):
+        if os.path.basename(f) != 'license.rtf': shutil.copy(f,tmp)
+
+    ##########################################################
+    print 'Compiling NSIS installer'
+    setup=r'..\downloads\metageta-%s-setup.exe'%outfile
+    cmd=r'makensis /V2 /DAPP_DIR=%s /DOUTPATH=%s /DVERSION=%s buildmetageta.nsi'%(tmp,setup,vers)
+    exit_code,stdout,stderr=runcmd(cmd)
+    if exit_code != 0:
+        if stderr and stdout:
+            sys.stderr.write(stderr)
+            sys.stdout.write(stdout)
+        elif stderr:  sys.stderr.write(stderr)
+        elif stdout:  sys.stdout.write(stdout)
+        else :        sys.stderr.write('NSIS installer compile failed')
+        #cleanup(tmp)
+        raw_input('Press enter to exit.')
+        sys.exit(exit_code)
+
+    ##########################################################
+    print 'Zipping files'
+    fout='../downloads/metageta-%s.zip'%outfile
+    zout=zip.ZipFile(fout,'w',zip.ZIP_DEFLATED)
+
+    #Code only    
+    for f in rglob(tmp):
+        if not f==sys.argv[0]:
+            zout.write(f,f.replace(tmp,'metageta/'))
+    zout.close()
+
+    #No installer
+    shutil.copyfile(fout,fout.replace('.zip','_pygdal.zip'))
+    zout=zip.ZipFile(fout.replace('.zip','_pygdal.zip'),'a',zip.ZIP_DEFLATED)
+    for f in rglob('../OSGeo4W'):
+        f=os.path.abspath(f)
+        zout.write(f,f.replace(td,'metageta/'))
+    zout.close()
+
+    #With installer
+    zout=zip.ZipFile(fout.replace('.zip','_pygdal-installer.zip'),'w',zip.ZIP_DEFLATED)
+    zout.write(setup, os.path.basename(setup))
+    zout.close()
+
+    cleanup(tmp)
+    raw_input('Press enter to exit.')
+
+def cleanup(*args):
+    for arg in args:
+        if os.path.isdir(arg):
+            try:shutil.rmtree(arg)
+            except Exception,err:
+                print err
+        elif os.path.isfile(arg):
+            try:os.remove(arg)
+            except Exception,err:
+                print err
+        else:pass
+
+def runcmd(cmd, format='s'):
+    ''' Run a command
+        @type     cmd:  C{str}
+        @param    cmd:  Command (inc arguments) to run
+        @rtype:   C{tuple}
+        @return:  Returns (exit_code,stdout,stderr)
+    '''
+    import subprocess
+    proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    if format.lower() == 's': #string output
+        stdout,stderr=proc.communicate()
+    #elif format.lower() == 'f': #file object output #doesn't flush IO buffer, causes python to hang
+    #    stdout,stderr=proc.stdout,proc.stderr
+    elif format.lower() == 'l': #list output
+        stdout,stderr=proc.stdout.readlines(),proc.stderr.readlines()
+    #else:raise TypeError, "fomat argument must be in ['s','f','l'] (string, file, list)"
+    else:raise TypeError, "fomat argument must be in ['s','l'] (string or list format)"
+    exit_code=proc.wait()
+    return exit_code,stdout,stderr
+
+def which(name, returnfirst=True, flags=os.F_OK | os.X_OK, path=None):
+    ''' Search PATH for executable files with the given name.
+    
+        On newer versions of MS-Windows, the PATHEXT environment variable will be
+        set to the list of file extensions for files considered executable. This
+        will normally include things like ".EXE". This fuction will also find files
+        with the given name ending with any of these extensions.
+
+        On MS-Windows the only flag that has any meaning is os.F_OK. Any other
+        flags will be ignored.
+        
+        Derived mostly from U{http://code.google.com/p/waf/issues/detail?id=531} with
+        additions from Brian Curtins patch - U{http://bugs.python.org/issue444582}
+
+        @type name: C{str}
+        @param name: The name for which to search.
+        @type returnfirst: C{boolean}
+        @param returnfirst: Return the first executable found.
+        @type flags: C{int}
+        @param flags: Arguments to U{os.access<http://docs.python.org/library/os.html#os.access>}.
+
+        @rtype: C{str}/C{list}
+        @return: Full path to the first matching file found or a list of the full paths to all files found, 
+                 in the order in which they were found.
+    '''
+    result = []
+    exts = filter(None, os.environ.get('PATHEXT', '').split(os.pathsep))
+    if not path:
+        path = os.environ.get("PATH", os.defpath)
+    for p in os.environ.get('PATH', '').split(os.pathsep):
+        p = os.path.join(p, name)
+        if os.access(p, flags):
+            if returnfirst:return p
+            else:result.append(p)
+        for e in exts:
+            pext = p + e
+            if os.access(pext, flags):
+                if returnfirst:return pext
+                else:result.append(pext)
+    return result
+
+class rglob:
+    '''A recursive/regex enhanced glob
+       adapted from os-path-walk-example-3.py - http://effbot.org/librarybook/os-path.htm 
+    '''
+    def __init__(self, directory, pattern="*", regex=False, regex_flags=0, recurse=True):
+        ''' @type    directory: C{str}
+            @param   directory: Path to xls file
+            @type    pattern: C{type}
+            @param   pattern: Regular expression/wildcard pattern to match files against
+            @type    regex: C{boolean}
+            @param   regex: Use regular expression matching (if False, use fnmatch)
+                            See U{http://docs.python.org/library/re.html}
+            @type    regex_flags: C{int}
+            @param   regex_flags: Flags to pass to the regular expression compiler.
+                                  See U{http://docs.python.org/library/re.html}
+            @type    recurse: C{boolean} 
+            @param   recurse: Recurse into the directory?
+        '''
+        self.stack = [directory]
+        self.pattern = pattern
+        self.regex = regex
+        self.recurse = recurse
+        self.regex_flags = regex_flags
+        self.files = []
+        self.index = 0
+
+    def __getitem__(self, index):
+        while 1:
+            try:
+                file = self.files[self.index]
+                self.index = self.index + 1
+            except IndexError:
+                # pop next directory from stack
+                
+                self.directory = self.stack.pop()
+                try:
+                    self.files = os.listdir(self.directory)
+                    self.index = 0
+                except:pass
+            else:
+                # got a filename
+                fullname = os.path.join(self.directory, file)
+                if os.path.isdir(fullname) and not os.path.islink(fullname) and self.recurse:
+                    self.stack.append(fullname)
+                if self.regex:
+                    import re
+                    if re.search(self.pattern,file,self.regex_flags):
+                        return fullname
+                else:
+                    import fnmatch
+                    if fnmatch.fnmatch(file, self.pattern):
+                        return fullname
+
+if __name__=='__main__':
+    main()
