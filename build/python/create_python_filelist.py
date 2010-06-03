ee2dcb2cc0e4079a74bf2e0612813fb580e32305
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2010-06-03 05:34:57 +0000

    Python filelist update

--- build/python/create_python_filelist.py
+++ build/python/create_python_filelist.py
@@ -1,5 +1,5 @@
 # -*- coding: latin-1 -*-
-import os,sys
+import os,sys,fnmatch
 sys.path.append('..')
 sys.path.append(os.path.dirname(os.path.dirname(sys.argv[0])))
 import setenv,utilities
@@ -7,6 +7,7 @@ import setenv,utilities
 pylist=open('python_filelist.txt','w')
 
 for f in sorted(utilities.rglob(setenv.PY_DIR, '*')):
-    if not os.path.isdir(f):
+    if not os.path.isdir(f) and not fnmatch.fnmatch(f,'*.py[c|o]'):
+        print f
         pylist.write(f.replace(setenv.BIN_DIR+'\\','')+'\n')
 pylist.close()
