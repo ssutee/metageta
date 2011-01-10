53ded8f341b258cf03273c5447b3a4f2262e976c
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2011-01-10 23:25:48 +0000

    Added usage and license to batchuploadmef.py

--- tools/batchuploadmef.py
+++ tools/batchuploadmef.py
@@ -1,3 +1,34 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2011 Australian Government, Department of Sustainability, Environment, Water, Population and Communities
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+'''
+Usage:
+batchuploadmef.py [options]
+Options:
+  -h, --help  show this help message and exit
+  -d dir      The directory to search for MEF files
+  -s site     Geonetwork site eg. firefly:8080
+  -u user     Geonetwork username
+  -p pass     Geonetwork password
+'''
 import sys
 def main(site,username,password,directory):
     import urllib2, urllib, cookielib, glob
