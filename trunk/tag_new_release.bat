fe807945abae95fc78a5219aaa310304874cce02
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2012-08-16 02:04:47 +0000

    fix version property setting

--- trunk/tag_new_release.bat
+++ trunk/tag_new_release.bat
@@ -14,7 +14,7 @@ SET SRC=https://metageta.googlecode.com/svn/branches/dsewpac/trunk
 SET DST=https://metageta.googlecode.com/svn/branches/dsewpac/tags/%version%
 
 svn copy %SRC% %DST% -m "Tagging version %version%"
-svn checkout --depth=empty %DST% %TEMP%\metageta-%version%
+svn checkout --depth=empty %DST% %TEMP%\metageta-dsewpac-%version%
 cd %TEMP%\metageta-%version%
 svn propset displayversion dsewpac-%version% .
 svn propset version %version%.$Revision$ .
