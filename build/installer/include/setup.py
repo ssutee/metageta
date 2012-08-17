192719e1419ac26cd4cebc155a0c2e1de297be68
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2012-08-17 06:35:46 +0000

    Fixing setup on linux

--- build/installer/include/setup.py
+++ build/installer/include/setup.py
@@ -98,5 +98,7 @@ if __name__=='__main__':
         print 'Changing mode of %s/bin/runcrawler|runtransform to 755'%data
         os.chmod(os.path.join(data,'bin/runcrawler.py'),stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
         os.chmod(os.path.join(data,'bin/runtransform.py'),stat.S_IRUSR|stat.S_IWUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP|stat.S_IROTH|stat.S_IXOTH)
-        os.link(os.path.join(data,'bin/runcrawler.py'),os.path.join(data,'bin/runcrawler'))
-        os.link(os.path.join(data,'bin/runtransform.py'),os.path.join(data,'bin/runtransform'))
+        try:
+            os.link(os.path.join(data,'bin/runcrawler.py'),os.path.join(data,'bin/runcrawler'))
+            os.link(os.path.join(data,'bin/runtransform.py'),os.path.join(data,'bin/runtransform'))
+        except:pass
