a1261ff50a796b5be27505566a832c83e6ab7357
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2012-08-17 06:56:31 +0000

    Tagging version 1.3.7.1

---
+++ tags/1.3.7.1/runtransform.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+call "%~DP0setenv.bat"
+
+REM Check if the progress bar GUI will be used.
+set nogui=0
+set /a nargs=0
+for %%a in (%*) do (
+    set /a nargs+=1
+    if /i "%%a"=="--nogui" set nogui=1
+    if /i "%%a"=="-h" (
+        set nogui=1
+    )
+    if /i "%%a"=="--help" (
+        set nogui=1
+    )
+)
+if %nogui%==1 (
+    call python.exe "%~DP0runtransform.py" %*
+    pause
+) else (
+    start "Crawler" /b pythonw.exe "%~DP0runtransform.py" %*
+)
+endlocal
