4a58350f6c649e2aeb67d034adfbc8a36604fa5f
pinner.luke <pinner.luke@c40560ac-0007-11df-bce7-934f6fdf4811>
2012-08-30 02:53:00 +0000

    Fix mef bug

--- tools/batchuploadmef.py
+++ tools/batchuploadmef.py
@@ -67,9 +67,9 @@ def main(site,username,password,directory,recurse):
 
         #Loop through the MEFs and upload them
         for mef in rglob(directory,'*.mef',recurse=recurse):
-            uploadmef(mef,handler,proxy,cj)
+            uploadmef(mef,site,handler,proxy,cj)
 
-def uploadmef(mef,handler,proxy,cj):
+def uploadmef(mef,site,handler,proxy,cj):
     #Set up the MEF import form values
     service='mef.import'
     formvalues={'insert_mode':'1',
